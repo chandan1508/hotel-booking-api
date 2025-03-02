6469b9a485905c023febd316cac6a2a75322a0db
Chandan <chandankuma8001360@gmail.com>
2025-03-02 23:47:29 +0530

    create model route and controller for hotel

--- index.js
+++ index.js
@@ -2,29 +2,43 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
-import usersRoute from "./routes/users.js"
-import hotelsRoute from "./routes/hotels.js"
-import roomsRoute from "./routes/rooms.js"
-const app = express()
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+const app = express();
 dotenv.config();
 
 const connect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO);
-        console.log("Connected to Database.");
-    } catch (error) {
-        throw error;
-    }
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to Database.");
+  } catch (error) {
+    throw error;
+  }
 };
 
 // middlewares
+
+app.use(express.json());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+// return res.status(500).json("hello error")
+});
 
-app.listen(8800, ()=>{
-    connect();
-    console.log("Connected to backend.");
-})
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend.");
+});
---
+++ routes/hotels.js
@@ -0,0 +1,25 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
+
+const router = express.Router();
+
+//Create
+router.post("/", createHotel);
+//Update
+router.put("/:id", updateHotel);
+//Delete
+router.delete("/:id", deleteHotel);
+//Get
+router.get("/:id", getHotel);
+//Get all
+router.get("/", getHotels);
+
+export default router;
