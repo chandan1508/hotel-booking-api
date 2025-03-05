6ee91bee4514a1585518c57d8e970951465fc9ce
Chandan <chandankuma8001360@gmail.com>
2025-03-06 00:17:36 +0530

    Implement authentication for users and hotels

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,28 @@
 import express from "express";
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req, res, next) => {
-    res.send("hello user, you are logged in")
-})
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//     res.send("hello user, you are logged in")
+// })
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//     res.send("hello user, you are logged in and you can delete your account")
+// })
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//     res.send("hello admin, you are logged in and you can delete your account")
+// })
 
 //Update
-router.put("/:id", updateUser);
+router.put("/:id",verifyUser, updateUser);
 //Delete
-router.delete("/:id", deleteUser);
+router.delete("/:id",verifyUser, deleteUser);
 //Get
-router.get("/:id", getUser);
+router.get("/:id",verifyUser, getUser);
 //Get all
-router.get("/", getUsers);
+router.get("/",verifyAdmin, getUsers);
 
 export default router
